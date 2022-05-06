34c87ae58c15d32fa8fae8d91f01bcd6d20b2dd2
leibin <leibin@jsjawlyxgs.wecom.work>
2022-05-06 21:27:05 +0800

    date 5.6

--- API/business/apiAdmin/apipost.py
+++ API/business/apiAdmin/apipost.py
@@ -3,54 +3,38 @@
 # Api层
 
 import requests
-class RequestHandler:1111
-    def get(self, url, **kwargs):
-        """封装get方法"""
-        # 获取请求参数
-        params = kwargs.get("params")
-        headers = kwargs.get("headers")
-        try:
-            result = requests.get(url, params=params, headers=headers)
-            return result
-        except Exception as e:
-            print("get请求错误: %s" % e)
-    def post(self, url, **kwargs):
-        """封装post方法"""
-        # 获取请求参数
-        params = kwargs.get("params")
-        data = kwargs.get("data")
-        json = kwargs.get("json")
-        try:
-            result = requests.post(url, params=params, data=data, json=json)
-            return result
-        except Exception as e:
-            print("post请求错误: %s" % e)
-    def run_main(self, method, **kwargs):
-        """
-        判断请求类型
-        :param method: 请求接口类型
-        :param kwargs: 选填参数
-        :return: 接口返回内容
-        """
-        if method == 'get':
-            result = self.get(**kwargs)
-            return result
-        elif method == 'post':
-            result = self.post(**kwargs)
-            return result
-        else:
-            print('请求接口类型错误')
-if __name__ == '__main__':
-    # 以下是测试代码
-    # get请求接口
-    url = 'https://api.apiopen.top/getJoke?page=1&count=2&type=video'
-    res = RequestHandler().get(url)
-    # post请求接口
-    url2 = 'http://127.0.0.1:8000/user/login/'
-    payload = {
-        "username": "vivi",
-        "password": "123456"
-    }
-    res2 = RequestHandler().post(url2,json=payload)
-    print(res.json())
-    print(res2.json())
+import json
+
+url = "https://test-bms.jaagro.com/jianan/yt-bms-bill/adjust/pay/bill/page"
+
+payload = json.dumps({
+  "auditStatus": "1",
+  "query": {
+    "current": 1,
+    "size": 10
+  }
+})
+headers = {
+  'Accept': 'application/json, text/plain, */*',
+  'Accept-Language': 'zh-CN,zh;q=0.9',
+  'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJ1c2VySWQiOjE0NDI3NjYxMjEzNzY2MDAwNjUsImFjY291bnQiOiJaSDIxMDkyODAwNDA3IiwicGhvbmUiOm51bGwsInV1aWQiOiI4OGIxZDVmNy03NGQ2LTQwY2QtOGEyNS01NTllNjQ4MmFmNWMiLCJzdWIiOiIxNDQyNzY2MTIxMzc2NjAwMDY1IiwiaWF0IjoxNjUxODAyMDg2LCJleHAiOjE2NTI0MDY4ODZ9.VL8az0a64pIPRTKGCD0VZgOJ06tEm_AsSa8fGmRMK20_VPL1vuBRYtn-DLaovkU_oWhUuIG5n847lvM7UGEEGQ',
+  'Connection': 'keep-alive',
+  'Content-Type': 'application/json',
+  'Cookie': 'UM_distinctid=17f9772d34e22c-028e2b2f7400cd-133a645d-13c680-17f9772d34fa1f',
+  'Origin': 'https://test-bms.jaagro.com',
+  'Referer': 'https://test-bms.jaagro.com/jaagro/bms/adjustManage/deliver/deliverQueryList',
+  'Sec-Fetch-Dest': 'empty',
+  'Sec-Fetch-Mode': 'cors',
+  'Sec-Fetch-Site': 'same-origin',
+  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36',
+  'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"',
+  'sec-ch-ua-mobile': '?0',
+  'sec-ch-ua-platform': '"macOS"'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
+
+
+
